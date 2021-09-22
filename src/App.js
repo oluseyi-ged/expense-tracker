8abbf644e03716496c9e279ad4cf2482230cea8a
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-22 16:26:22 +0100

    boiler plate setup of components and lil styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Grid } from "@material-ui/core"
+import Details from "./components/Details/Details.jsx"
 
-function App() {
+import useStyles from "./styles"
+
+const App = () => {
+  const styles = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid
+        className="grid"
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          Main
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
