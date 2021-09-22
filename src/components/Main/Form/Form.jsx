ba284c30a21eea29dfbd39b12844919c1bf38c2e
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-22 19:53:55 +0100

    speechly configured and implemented

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import {
   TextField,
   Typography,
@@ -11,6 +11,7 @@ import {
 } from "@material-ui/core"
 import { ExpenseTrackerContext } from "../../../context/context"
 import { v4 as uuidv4 } from "uuid"
+import { useSpeechContext } from "@speechly/react-client"
 
 import formatDate from "../../../utils/formatDate"
 import useStyles from "./styles.js"
@@ -29,8 +30,12 @@ const initialState = {
 const Form = () => {
   const [formData, setFormData] = useState(initialState)
   const { addTransaction } = useContext(ExpenseTrackerContext)
+  const { segment } = useSpeechContext()
 
   const createTransaction = () => {
+    if (Number.isNaN(Number(formData.amount)) || !formData.date.includes("-"))
+      return
+
     const transaction = {
       ...formData,
       amount: Number(formData.amount),
@@ -39,6 +44,60 @@ const Form = () => {
     addTransaction(transaction)
     setFormData(initialState)
   }
+
+  useEffect(() => {
+    if (segment) {
+      if (segment.intent.intent === "add_expense") {
+        setFormData({ ...formData, type: "Expense" })
+      } else if (segment.intent.intent === "add_income") {
+        setFormData({ ...formData, type: "Income" })
+      } else if (
+        segment.isFinal &&
+        segment.intent.intent === "create_transaction"
+      ) {
+        return createTransaction()
+      } else if (
+        segment.isFinal &&
+        segment.intent.intent === "cancel_transaction"
+      ) {
+        return setFormData(initialState)
+      }
+
+      segment.entities.forEach((s) => {
+        const category = `${s.value.charAt(0)}${s.value.slice(1).toLowerCase()}`
+
+        switch (s.type) {
+          case "amount":
+            setFormData({ ...formData, amount: s.value })
+            break
+          case "category":
+            if (incomeCategories.map((iC) => iC.type).includes(category)) {
+              setFormData({ ...formData, type: "Income", category })
+            } else if (
+              expenseCategories.map((iC) => iC.type).includes(category)
+            ) {
+              setFormData({ ...formData, type: "Expense", category })
+            }
+            break
+          case "date":
+            setFormData({ ...formData, date: s.value })
+            break
+          default:
+            break
+        }
+      })
+
+      if (
+        segment.isFinal &&
+        formData.amount &&
+        formData.category &&
+        formData.type &&
+        formData.date
+      ) {
+        createTransaction()
+      }
+    }
+  }, [segment])
 
   const classes = useStyles()
 
@@ -49,7 +108,7 @@ const Form = () => {
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Typography align="center" variant="subtitle2" gutterBottom>
-          ...
+          {segment && segment.words.map((w) => w.value).join("")}
         </Typography>
       </Grid>
       <Grid item xs={6}>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import "./Index.css"
 import { Provider } from "./context/context"
+import { SpeechProvider } from "@speechly/react-client"
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider appId="e0961da1-d6a7-43f6-8b35-92175ef3caad" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById("root")
 )
