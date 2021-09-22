0f927c42f77095d5d87093f979428630ff081092
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-22 20:09:49 +0100

    edit totalview on main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import {
   Card,
   CardContent,
@@ -9,18 +9,20 @@ import {
 } from "@material-ui/core"
 import Form from "./Form/Form"
 import List from "./List/List"
+import { ExpenseTrackerContext } from "../../context/context"
 
 import useStyles from "./styles.js"
 
 const Main = () => {
   const classes = useStyles()
+  const { balance } = useContext(ExpenseTrackerContext)
 
   return (
     <Card className={classes.root}>
       <CardHeader title="Expense Tracker" subtitle="Speechly Integrated" />
       <CardContent>
         <Typography align="center" variant="h5">
-          Total Balance $100
+          Total Balance ${balance}
         </Typography>
         <Typography
           variant="subtitle1"
